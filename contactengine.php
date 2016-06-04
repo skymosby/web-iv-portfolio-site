7d1062f627675856db116fb46e7864d1c82c16e0
skymosby <skylarmosby@Skylars-MacBook-Pro.local>
2016-06-04 11:48:31 -0500

    forms and bug-fixin
    I added the php for my contact form and meta data for my project pages
    to make them work on mobile.

---
+++ contactengine.php
@@ -0,0 +1,43 @@
+<?php
+
+$EmailFrom = "butts@email.com";
+$EmailTo = "hello@skymosby.com";
+$Subject = "skymosby.com contact form submission";
+$First = Trim(stripslashes($_POST['First']));
+$Last = Trim(stripslashes($_POST['Last'])); 
+$Email = Trim(stripslashes($_POST['Email'])); 
+$Message = Trim(stripslashes($_POST['Message'])); 
+
+// validation
+$validationOK=true;
+if (!$validationOK) {
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=error.htm\">";
+  exit;
+}
+
+// prepare email body text
+$Body = "";
+$Body .= "First Name: ";
+$Body .= $First;
+$Body .= "\n";
+$Body .= "Last Name: ";
+$Body .= $Last;
+$Body .= "\n";
+$Body .= "Email: ";
+$Body .= $Email;
+$Body .= "\n";
+$Body .= "Message: ";
+$Body .= $Message;
+$Body .= "\n";
+
+// send email 
+$success = mail($EmailTo, $Subject, $Body, "From: <$EmailFrom>");
+
+// redirect to success page 
+if ($success){
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=contactthanks.php\">";
+}
+else{
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=error.htm\">";
+}
+?>
---
+++ contactthanks.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html class="no-js" lang="en" dir="ltr">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="x-ua-compatible" content="ie=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Thanks!</title>
+<link rel="stylesheet" href="css/foundation.css">
+<link rel="stylesheet" href="css/app.css">
+<link rel="stylesheet" href="css/main.css">
+<link href="https://fonts.googleapis.com/css?family=Poppins:400,300,500,600,700" rel="stylesheet" type="text/css">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js"></script>
+</head>
+
+<body>
+
+	<div id="page-wrap">
+
+	
+			
+			
+		<br /><br />
+		
+			
+		<h1>Thank You</h1><br />
+		<p>Your message has bee sent. I'll get back to you in a jiffy!</p>
+		
+		<p><a href="index.html">Home</a></p>
+	
+	</div>
+	
+	<script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
+	</script>
+	<script type="text/javascript">
+	_uacct = "UA-68528-29";
+	urchinTracker();
+	</script>
+
+</body>
+
+</html>
